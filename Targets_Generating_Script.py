d87678a176c950bbcbd59651d1e78986116cddc8
Jordan Foss <j.foss@uqconnect.edu.au>
2021-04-29 13:44:53 +1000

    Big Boy CSVs

--- Targets_Generating_Script.py
+++ Targets_Generating_Script.py
@@ -7,16 +7,23 @@ Created on Sun Apr 25 18:33:13 2021
 """
 
 import subprocess
-    
-maleLocations = ["/home/jordan/Documents/UniversityStuff/2021/Stat3007/STAT3007_Project/Audio_Speech_Actors_01-24/Male/"
+#Change local use right click, click propertites then copy the file path to replace the below string
+github_path = "/home/jordan/Documents/UniversityStuff/2021/Stat3007/STAT3007_Project/"
+
+maleLocations = [github_path + "Audio_Speech_Actors_01-24/Male/"
              + x for x in 
              ["Actor_" + y + "/" for y in ["01", "03", "05", "07", "09", "11", "13", "15", "17", "19", "21", "23"]]]
 
-femaleLocations = ["/home/jordan/Documents/UniversityStuff/2021/Stat3007/STAT3007_Project/Audio_Speech_Actors_01-24/Female/"
+femaleLocations = [github_path + "Audio_Speech_Actors_01-24/Female/"
              + x for x in 
              ["Actor_" + y + "/" for y in ["02", "04", "06", "08", "10", "12", "14", "16", "18", "20", "22", "24"]]]
 
 locations = maleLocations + femaleLocations
+
+big_file = open(github_path + "big_csv.csv", 'w')
+
+big_file.write("Filenames,Emotion,Intensity,Phrase")
+big_file.write("\n")
 
 
 for location in locations:
@@ -27,11 +34,14 @@ for location in locations:
     filenames.pop()
     print(filenames)
     
-    targets = []
+    file.write("Filenames,Emotion,Intensity,Phrase")
+    file.write("\n")
     
     for name in filenames:
-        targets.append([name[1], name[4], name[7]])
-        
-    file.write(str(targets))
+        file.write(name + "," + name[1] + "," + name[4] + "," + name[7])
+        file.write("\n")
+        big_file.write(name + "," + name[1] + "," + name[4] + "," + name[7])
+        big_file.write("\n")
     
     file.close()
+big_file.close()
